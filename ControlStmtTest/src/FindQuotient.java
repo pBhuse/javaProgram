5f889424468dbe111d50ea07b80ace750243758a
pBhuse <pratikshabhuse2002@gmail.com>
2024-01-25 16:58:04 +0530

    Find Quotient And Remainder
    Find Quotient And Remainder

---
+++ ControlStmtTest/src/FindQuotient.java
@@ -0,0 +1,16 @@
+
+public class FindQuotient {
+public static void main(String[] args) {
+	System.out.println("========= Q2 Find Quotient And Remainder ========");
+	int num1=20;
+	System.out.println("Num1="+num1);
+	int num2=5;
+	System.out.println("Num2="+num2);
+
+	int res1=num1/num2;
+	System.out.println("Quotient="+res1);
+	int res2=num1%num2;
+	System.out.println("Remainder="+res2);
+
+}
+}
